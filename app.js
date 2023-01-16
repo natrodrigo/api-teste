4ae0e8c265c04dfbbcca04d40b7aaa9a1fc24393
Natã Rodrigo Santos Pereira <nata.rodrigo.p@gmail.com>
2023-01-16 16:40:18 -0300

    creating initial api

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors')
-const path = require('path');
+
 
 
 const app = express();
-app.use(express.static(__dirname));
 
 const PORT = 5000 || process.env.PORT;
 const HOST = '0.0.0.0';
@@ -12,7 +11,8 @@ const HOST = '0.0.0.0';
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+"/index.html"))
+    const valor = Math.ceil(Math.random()*100);
+    res.json({"valor_produto": valor})
 })
 
 
